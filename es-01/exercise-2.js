7da0e112c6aab15fc2466cdedf527f30c9c72d52
dina <timcud6@gmail.com>
2023-05-31 19:41:20 +0300

    added ex 1

--- es-01/exercise-2.js
+++ es-01/exercise-2.js
@@ -4,7 +4,25 @@
 // developer => 1500
 // default => 1000
 function calculateSalary(role) {
-  // ...
+  let salary;
+  switch (role) {
+    case "ceo":
+      salary = 2200;
+      break;
+    case 'manager':
+      salary = 1800;
+      break;
+    case 'cto':
+      salary = 1800;
+      break;
+    case "developer":
+      salary = 1500;
+      break;
+    default:
+      salary = 1000;
+  }
+  return salary + '€';
+
 }
 
 const ceoSalary = calculateSalary('ceo');
