c50bd8234a338de68af4be6679d23c137cd42e60
dina <timcud6@gmail.com>
2023-06-01 08:04:57 +0300

    added ex 6

---
+++ exercise 6/exercise.js
@@ -0,0 +1,13 @@
+function printName() {
+    let helloName = "Hello John";
+    
+    function inner() {
+      return helloName;
+    }
+    
+    return inner;
+  }
+  
+  let closure = printName();
+  
+  console.log(closure());
