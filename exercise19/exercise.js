3f0b5ad9f9418408c2f2918527d3b68f42085d98
dina <timcud6@gmail.com>
2023-06-12 01:04:50 +0300

    added ex19

---
+++ exercise19/exercise.js
@@ -0,0 +1,38 @@
+class Person {
+  #firstName;
+  #lastName;
+  #age;
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  get firstName() {
+    return this.#firstName;
+  }
+
+  set firstName(value) {
+    this.#firstName = value;
+  }
+
+  get lastName() {
+    return this.#lastName;
+  }
+
+  set lastName(value) {
+    this.#lastName = value;
+  }
+
+ 
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+const person = new Person("Mario", "Rossi", 25);
+console.log(person.fullName); 
+
+person.firstName = "Maria";
+person.lastName = "Verdi";
+console.log(person.fullName);
