f6695c3953e9b2efe231fba00a11281c8b3683d8
dina <timcud6@gmail.com>
2023-06-28 17:19:06 +0300

    added call

---
+++ Callbacks 1/exercise.js
@@ -0,0 +1,10 @@
+function printAsyncName(callback, name) {
+  setTimeout(() => {
+    console.log('Hello');
+    setTimeout(() => {
+      console.log(name);
+    }, 2000);
+  }, 1000);
+}
+
+printAsyncName(() => {}, 'Mario');
