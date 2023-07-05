f1dd1a119c60d584b0be19476b31eaf4846c7981
dina <timcud6@gmail.com>
2023-07-05 19:35:41 +0300

    added file

--- Callbacks 2/exercise.js
+++ Callbacks 2/exercise.js
@@ -1,10 +1,12 @@
 function repeatHello(callback){
   setInterval (() => {
-    console.log('HELLO');
+    callback('HELLO');
   }, 1000)
 
 }
 
-repeatHello();
+repeatHello((message) => {
+  console.log(message);
+});
 
 //La funzione freccia è una sintassi più concisa per definire una funzione in JavaScript e può essere utilizzata in modo simile a una funzione normale.
